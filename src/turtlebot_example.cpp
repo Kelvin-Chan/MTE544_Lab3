911ba9b12f9495efcc30491547162e609ca0adfe
Nick Manna <manna422@gmail.com>
2017-11-27 16:27:29 -0500

    AStar WIP

--- src/turtlebot_example.cpp
+++ src/turtlebot_example.cpp
@@ -19,6 +19,7 @@
 
 // Additional Libraries
 #include <Eigen/Dense>
+#include <Eigen/Sparse>
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -52,8 +53,10 @@ static const int wp_sequence[] = {1,3,1,2};
 Matrix<int,Dynamic,Dynamic,RowMajor> grid_map;
 
 // Milestones (Col0: X, Col1: Y)
-MatrixXd Milestones = MatrixXd::Zero(num_milestones, 2);
-Matrix<bool, num_milestones, num_milestones ,RowMajor> MilestoneEdges;
+Matrix<int, num_milestones, 2, RowMajor> Milestones;
+Matrix<bool, num_milestones, num_milestones, RowMajor> MilestoneEdges;
+// Matrix<double, num_milestones, num_milestones, RowMajor> EdgeDistances;
+SparseMatrix<double> EdgeDistances(num_milestones, num_milestones);
 
 
 // Waypoints [x[m], y[m], θ[rad]]
@@ -79,8 +82,8 @@ geometry_msgs::Twist vel;
 
 // ------------------------------------------------------------------
 // Macros
-#define D2C_DISTANCE(x) double(x*25.0/100.0 - (25.0/2.0))
-#define C2D_DISTANCE(x) int((x + (25.0/2.0)) * 100.0/25.0)
+#define D2C_DISTANCE(x) double(x*10.0/100.0 - (10.0/2.0))
+#define C2D_DISTANCE(x) int((x + (10.0/2.0)) * 100.0/10.0)
 // ------------------------------------------------------------------
 
 //Callback function for the Position topic (LIVE)
@@ -187,6 +190,8 @@ void generate_milestones()
     int current_x;
     int current_y;
 
+    std::cout << "\% Generating Milestones"<< std::endl;
+
     for (int i = 0; i < num_milestones; i++)
     {
         while (true) {
@@ -199,6 +204,18 @@ void generate_milestones()
         Milestones(i, 0) = current_x;
         Milestones(i, 1) = current_y;
     }
+
+    // overwrite first four positions with initial IPS and 3 Waypoints
+    // XXX: Placeholder for IPS x,y
+    for (int i = 1; i < 4; i++)
+    {
+        Milestones(i, 0) = C2D_DISTANCE(wp_list[i-1][0]);
+        Milestones(i, 1) = C2D_DISTANCE(wp_list[i-1][1]);
+    }
+
+    Milestones(0, 0) = 8;
+    Milestones(0, 1) = 8;
+
 }
 
 void visualize_milestones()
@@ -224,6 +241,7 @@ double edge_length(int i, int j)
 
 void generate_edges()
 {
+    std::cout << "\% Generating Edges"<< std::endl;
     // MilestoneEdges
     int i, j;
 
@@ -278,6 +296,12 @@ void generate_edges()
                     break;
                 }
             }
+
+            if (MilestoneEdges(i,j))
+            {
+                EdgeDistances.insert(i,j) = edge_length(i,j);
+                EdgeDistances.insert(j,i) = EdgeDistances.coeff(i,j);
+            }
         }
     }
 }
@@ -298,28 +322,169 @@ void visualize_edges()
 }
 
 
-void generate_path_partial(float X0, float Y0, float X1, float Y1)
+// returns index of smallest double in vec
+int find_vec_min(std::vector<double> &vec)
 {
-    std::cout << "\% Routing (" << X0 << "," << Y0 << ") - (" << X1 << "," << Y1 << ")" << std::endl;
-    int x0 = C2D_DISTANCE(X0);
-    int y0 = C2D_DISTANCE(Y0);
-    int x1 = C2D_DISTANCE(X1);
-    int y1 = C2D_DISTANCE(Y1);
+    int min_ind = 0;
+    double min_val = vec[0];
+    for (int i = 1; i < vec.size(); i++)
+    {
+        if (vec[i] < min_val)
+        {
+            min_ind = 0;
+            min_val = vec[i];
+        }
+    }
+    return min_ind;
+}
+
+// look for val in vec and return its index
+int find_vec_ind(std::vector<int> &vec, int val)
+{
+    for (int i = 0; i < vec.size(); i++)
+    {
+        if (vec[i] == val)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+// Implementation of A* algorithm
+// start - index of start milestone
+// finish - index of finish milestone
+void a_star_algorithm(int start, int finish)
+{
+    bool done = false;
+    double dmax = edge_length(start, finish);// EdgeDistances(start,finish)
+
+    std::vector<int>    open_node;
+    std::vector<int>    open_backtrack;
+    std::vector<double> open_lower_cost;
+    std::vector<double> open_current_cost;
+
+    std::vector<int>    closed_node;
+    std::vector<int>    closed_backtrack;
+    std::vector<double> closed_lower_cost;
+    std::vector<double> closed_current_cost;
+
+    open_node.push_back(start);
+    open_backtrack.push_back(-1);
+    open_lower_cost.push_back(dmax);
+    open_current_cost.push_back(0.0);
+
+    int best;
+    int best_milestone;
+    int i;
+    double dtogo;
+    double dcurr;
+    int temp_open_node_index;
+
+    // Main Algorithm
+    while(!done)
+    {
+        cout << open_node.size() << " - " << closed_node.size() << endl;
+        if (open_node.empty())
+        {
+            // XXX
+            return;
+        }
+
+        // find node with lowest cost node
+        best = find_vec_min(open_lower_cost);
+
+        // move node to close set
+        closed_node.push_back(open_node[best]);
+        closed_backtrack.push_back(open_backtrack[best]);
+        closed_lower_cost.push_back(open_lower_cost[best]);
+        closed_current_cost.push_back(open_current_cost[best]);
+
+        open_node.erase(open_node.begin() + best);
+        open_backtrack.erase(open_backtrack.begin() + best);
+        open_lower_cost.erase(open_lower_cost.begin() + best);
+        open_current_cost.erase(open_current_cost.begin() + best);
+
+        best_milestone = closed_node.back();
+        // Check for end condition
+        if (best_milestone == finish)
+        {
+            done = 1;
+            continue;
+        }
+
+        // interate over connected milestones
+        for (i = 0; i < num_milestones; i++)
+        {
+            // check for edge
+            if (MilestoneEdges(best_milestone, i))
+            {
+                // if milestone in closed set, continue
+                if (find_vec_ind(closed_node, i) != -1)
+                {
+                    continue;
+                }
+                dtogo = edge_length(i, finish);
+                dcurr = closed_current_cost.back() + EdgeDistances.coeff(best_milestone, i);
+
+                // if connect milestone in open set, update distance
+                // otherwise append it to open set
+                temp_open_node_index = find_vec_ind(open_node, i);
+                if (temp_open_node_index == -1)
+                {
+                    open_node.push_back(i);
+                    open_backtrack.push_back(best_milestone);
+                    open_lower_cost.push_back(dtogo+dcurr);
+                    open_current_cost.push_back(dcurr);
+                }
+                else
+                {
+                    open_backtrack[temp_open_node_index] = best_milestone;
+                    open_lower_cost[temp_open_node_index] = dtogo+dcurr;
+                    open_current_cost[temp_open_node_index] = dcurr;
+                }
+
+            }
+        }
+
+    }
+
+    // Backtrack and find final path
+    int current_milestone = finish;
+    while (current_milestone != start)
+    {
+    }
+    int current_milestone_close_node_ind = find_vec_ind(closed_node, finish);
+    int prev =
 
 }
 
+// void generate_path_partial(float X0, float Y0, float X1, float Y1)
+// {
+//     std::cout << "\% Routing (" << X0 << "," << Y0 << ") - (" << X1 << "," << Y1 << ")" << std::endl;
+//     int x0 = C2D_DISTANCE(X0);
+//     int y0 = C2D_DISTANCE(Y0);
+//     int x1 = C2D_DISTANCE(X1);
+//     int y1 = C2D_DISTANCE(Y1);
+// 
+// }
+
 void generate_path()
 {
+    std::cout << "\% Generating Path " << 0 << " For: IPS - " << wp_sequence[0] << std::endl;
+
     int num_wp = sizeof(wp_sequence)/sizeof(wp_sequence[0]);
     for (int i = 0; i < (num_wp - 1); i++)
     {
-        std::cout << "\% Generating Path " << i << " For: "<< wp_sequence[i] << " - " << wp_sequence[(i+1)] << std::endl;
-        generate_path_partial(
-            wp_list[(wp_sequence[i]-1)][0],
-            wp_list[(wp_sequence[i]-1)][1],
-            wp_list[(wp_sequence[i+1]-1)][0],
-            wp_list[(wp_sequence[i+1]-1)][1]
-        );
+        std::cout << "\% Generating Path " << (i+1) << " For: "<< wp_sequence[i] << " - " << wp_sequence[(i+1)] << std::endl;
+        a_star_algorithm(wp_sequence[i], wp_sequence[(i+1)]);
+        // generate_path_partial(
+        //     wp_list[(wp_sequence[i]-1)][0],
+        //     wp_list[(wp_sequence[i]-1)][1],
+        //     wp_list[(wp_sequence[i+1]-1)][0],
+        //     wp_list[(wp_sequence[i+1]-1)][1]
+        // );
     }
 }
 
